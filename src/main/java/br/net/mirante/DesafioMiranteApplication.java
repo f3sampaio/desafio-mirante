161313cce17db3d8f92627d38590687778b3aa52
Felipe Sampaio Marques <fmarques899@gmail.com>
2020-05-22 15:12:50 -0300

    Initializing project repo

---
+++ src/main/java/br/net/mirante/DesafioMiranteApplication.java
@@ -0,0 +1,13 @@
+package br.net.mirante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioMiranteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioMiranteApplication.class, args);
+	}
+
+}
